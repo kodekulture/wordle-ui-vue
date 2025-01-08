cb0ec4e11a8a9d802dc08a2f7b85be9d757c5713
Evans Owamoyo <oovamoyo@ozon.ru>
2025-01-08 20:41:09 +0300

    fix(ui): unresponsive clicks on page

--- pages/game/[id].vue
+++ pages/game/[id].vue
@@ -1,28 +1,39 @@
 <template>
-  <div class="relative p-2 h-full flex flex-col">
+  <div class="relative p-2 h-full flex flex-col min-h-0">
     <!-- Large and Medium Screen Layout -->
-    <div class="hidden md:flex h-full w-full">
-      <game-board :game="game" class="flex-grow md:w-1/2 flex justify-between items-center" />
+    <div class="hidden md:flex h-full w-full min-h-0">
+      <!-- Game Board -->
+      <game-board
+          :game="game"
+          class="flex-grow md:w-1/2 flex justify-between items-center min-h-0 overflow-auto"
+      />
 
-      <UDivider orientation="vertical" />
-    <!-- Medium Screen Layout: Chat and Leaderboard Stacked Vertical -->
-    <div class="hidden md:flex md:flex-col md:w-1/2 lg:w-1/3">
-      <!-- Leaderboard Section -->
-      <game-leader-board />
-      <UDivider class="block lg:hidden" size="md" />
-      <!-- Medium Screen ONLY: Chat Screen (Bottom) -->
-      <game-chat v-if="!ended" class="flex-1 grow shrink-0 flex justify-center items-center lg:hidden" />
+      <UDivider orientation="vertical"/>
+      <!-- Medium Screen Layout: Chat and Leaderboard Stacked Vertical -->
+      <div class="hidden md:flex md:flex-col md:w-1/2 lg:w-1/3 min-h-0">
+        <!-- Leaderboard Section -->
+        <game-leader-board class="flex-1 min-h-0 overflow-auto"/>
+        <UDivider class="block lg:hidden" size="md"/>
+        <!-- Medium Screen ONLY: Chat Section -->
+        <game-chat
+            v-if="!ended"
+            class="flex-1 grow shrink-0 flex justify-center items-center lg:hidden min-h-0 overflow-auto"
+        />
+      </div>
+
+      <!-- Large Screen Layout only: Chat Section -->
+      <game-chat
+          v-if="!ended"
+          class="hidden lg:flex lg:w-1/3 bg-black-500 justify-center items-center min-h-0 overflow-auto"
+      />
     </div>
-    <!-- Large Screen Layout only: Chat part -->
-    <game-chat v-if="!ended" class="hidden lg:flex lg:w-1/3 bg-black-500 justify-center items-center" />
-    </div>
-    <UModal v-model="showLoading" :transition="false" prevent-close>
+    <UModal v-model="showLoading" prevent-close>
       <div class="p-4 flex flex-col items-center justify-center">
         <span class="mb-4">Loading...</span>
         <UProgress animation="carousel" />
       </div>
     </UModal>
-    <UModal v-model="showError" :transition="false" prevent-close>
+    <UModal v-model="showError" prevent-close>
         <div class="p-4 flex flex-col justify-center">
           <span class="text-wrap mb-4">{{shownError}}</span>
           <UButton @click="navigateTo('/game')" class="justify-center">Exit</UButton>
@@ -43,16 +54,21 @@
       </div>
     </UModal>
     <!-- Mobile Layout -->
-    <div class="block h-full md:hidden">
-      <!-- Game Section -->
-      <game-board :game="game" class="h-full flex justify-between items-center" />
+    <div class="block h-full md:hidden min-h-0">
+      <!-- Game Board -->
+      <game-board
+          :game="game"
+          class="h-full flex justify-between items-center min-h-0 overflow-auto"
+      />
+
       <!-- Floating Buttons -->
       <div class="absolute top-4 right-4 flex flex-col gap-2 z-50">
-        <UButton @click="activeOverlay = 'chat'" label="Chats" />
-        <UButton @click="activeOverlay = 'leaderboard'" label="LeaderBoard" />
+        <UButton @click="activeOverlay = 'chat'" label="Chats"/>
+        <UButton @click="activeOverlay = 'leaderboard'" label="LeaderBoard"/>
       </div>
 
-      <LazyUSlideover v-model="overlayPresent">
+      <!-- Overlay -->
+      <USlideover v-model="overlayPresent">
         <UButton
             color="gray"
             variant="ghost"
@@ -63,58 +79,61 @@
             padded
             @click="activeOverlay = null"
         />
-        <div v-if="activeOverlay == 'chat'">
-          <lazy-game-chat />
+        <div v-if="activeOverlay == 'chat'" class="min-h-0 overflow-auto">
+          <game-chat/>
         </div>
-        <div v-if="activeOverlay == 'leaderboard'">
-          <lazy-game-leader-board />
+        <div
+            v-if="activeOverlay == 'leaderboard'"
+            class="min-h-0 overflow-auto"
+        >
+          <game-leader-board/>
         </div>
-      </LazyUSlideover>
-
+      </USlideover>
     </div>
   </div>
 </template>
 
+
 <script lang="ts" setup>
 // Game LOGIC
-  import { storeToRefs } from 'pinia'
-  import { computed } from 'vue'
-  import { useRoute } from 'vue-router'
-  import {useFinishedGameStore} from "~/stores/finishedGameStore";
+import {storeToRefs} from 'pinia'
+import {computed} from 'vue'
+import {useRoute} from 'vue-router'
+import {useFinishedGameStore} from "~/stores/finishedGameStore";
 
 
-  const {params, query} = useRoute()
-  const id = params.id
-  const finished = query.finished
+const {params, query} = useRoute()
+const id = params.id
+const finished = query.finished
 
-  const store = useGameStore()
-  const { game, error:finished_error } = storeToRefs(useFinishedGameStore())
-  if (!finished) {
-    store.join(id)
-  }
+const store = useGameStore()
+const {game, error: finished_error} = storeToRefs(useFinishedGameStore())
+if (!finished) {
+  store.join(id)
+}
 
-  const { loading, error, active, owner } = storeToRefs(store)
-  const notActive = computed(() => !loading.value && !active.value)
-  const ended = computed<boolean>(() => game.value?.ended_at != null)
-  const showLoading = computed(() => ended.value ? false : loading.value)
+const {loading, error, active, owner} = storeToRefs(store)
+const notActive = computed(() => !loading.value && !active.value)
+const ended = computed<boolean>(() => game.value?.ended_at != null)
+const showLoading = computed(() => ended.value ? false : loading.value)
 
-  const shownError = computed<string | null>(() => {
-    return ended.value ? finished_error.value : error.value
-  })
+const shownError = computed<string | null>(() => {
+  return ended.value ? finished_error.value : error.value
+})
 
-  const showError = computed<boolean>(() => shownError.value != null)
-  const showStart = computed<boolean>(() => !active.value && !showError.value && !showLoading.value)
+const showError = computed<boolean>(() => shownError.value != null)
+const showStart = computed<boolean>(() => !active.value && !showError.value && !showLoading.value)
 
-  watch(error, () => {
-    if (error.value == null) return;
-    showToastError(error.value)
-  })
+watch(error, () => {
+  if (error.value == null) return;
+  showToastError(error.value)
+})
 
-  definePageMeta({
-    middleware: ['auth', 'finished-game']
-  })
+definePageMeta({
+  middleware: ['auth', 'finished-game']
+})
 
-  // Display LOGIC
+// Display LOGIC
 const activeOverlay = ref<null | 'chat' | 'leaderboard'>(null)
 const overlayPresent = computed(() => activeOverlay.value != null)
 </script>
